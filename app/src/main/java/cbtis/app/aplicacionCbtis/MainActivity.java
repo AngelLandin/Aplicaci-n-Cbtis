b6b90cc891623987c26edb3fa522c4678253403a
Angel Landin Lopez <angel.ezlan@gmail.com>
2022-08-23 05:24:53 -0500

    Interfaz-inicio-sesion terminada

---
+++ app/src/main/java/cbtis/app/aplicacionCbtis/MainActivity.java
@@ -0,0 +1,17 @@
+package cbtis.app.aplicacionCbtis;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Escribir codigo.
+}
