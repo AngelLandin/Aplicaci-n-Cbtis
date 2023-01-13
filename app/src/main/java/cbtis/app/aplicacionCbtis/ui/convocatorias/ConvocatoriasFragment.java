af675ebbbeb920e16ed223af1ce7919c492b7d73
Angel Landin Lopez <angel.ezlan@gmail.com>
2023-01-12 19:44:28 -0600

    logo de la app agregada

--- app/src/main/java/cbtis/app/aplicacionCbtis/ui/convocatorias/ConvocatoriasFragment.java
+++ app/src/main/java/cbtis/app/aplicacionCbtis/ui/convocatorias/ConvocatoriasFragment.java
@@ -51,6 +51,7 @@ public class ConvocatoriasFragment extends Fragment implements RecyclerViewInter
         elementos = new ArrayList<>();
         elementos.add(new ListElement("SANTANDER", "Santander Scholarships Language | UK English Summer Experience 2023", "Si quieres pasar tres semanas en un campus universitario británico este verano, Banco Santander, en colaboración con British Council, ofrece 100 becas", "https://app.becas-santander.com/es/program/santander-scholarships-language-uk-english-summer-experience-2023"));
         elementos.add(new ListElement("Tecnológico de Monterrey", "Lideres del mañana", "Es un programa de movilidad social para formar líderes talentosos y sensibles que impacten positivamente a la comunidad, y que requieren apoyo financiero del 100 % para estudiar una carrera profesional en el Tec de Monterrey.", "https://lideresdelmanana.itesm.mx/"));
+        elementos.add(new ListElement("Santander", "Becas Santander Estudios | Fundación Fibra Educa (FEDUCA) 2023", "El Programa “Becas Santander – Estudios – Fundación Fibra Educa (FEDUCA) 2023” (en lo sucesivo “el Programa”) es una iniciativa de Fundación Fibra Educa", "https://app.becas-santander.com/es/program/becas-santander-estudios-fundacion-fibra-educa-feduca-2023?category=STUDIESstatus%3DopenoriginCountry%3DMX&track=search"));
 
         ListAdapter listAdapter = new ListAdapter(elementos, getContext(), this);
         RecyclerView recyclerView = view.findViewById(R.id.recycler_convocatorias);
