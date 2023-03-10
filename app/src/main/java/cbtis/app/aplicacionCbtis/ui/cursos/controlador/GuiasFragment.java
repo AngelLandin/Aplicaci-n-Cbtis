eb7b93e04e66cf2cb549be2c77daf7e10d96d615
Angel Landin Lopez <angel.ezlan@gmail.com>
2023-03-10 10:38:58 -0600

    Guias de estudio agregadas

--- app/src/main/java/cbtis/app/aplicacionCbtis/ui/cursos/controlador/GuiasFragment.java
+++ app/src/main/java/cbtis/app/aplicacionCbtis/ui/cursos/controlador/GuiasFragment.java
@@ -1,10 +1,13 @@
 package cbtis.app.aplicacionCbtis.ui.cursos.controlador;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,7 +15,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
@@ -22,23 +27,22 @@ import com.google.firebase.storage.StorageReference;
 import java.util.ArrayList;
 
 import cbtis.app.aplicacionCbtis.R;
-import cbtis.app.aplicacionCbtis.ui.cursos.adapterGuias.ListAdapter;
-import cbtis.app.aplicacionCbtis.ui.cursos.adapterGuias.RecyclerViewInterfaceGuias;
+import cbtis.app.aplicacionCbtis.ui.cursos.cuadernillos.CuadernillosCuartoSActivity;
+import cbtis.app.aplicacionCbtis.ui.cursos.cuadernillos.VisoresPDF.VisorPDFCuartoActivity;
+import cbtis.app.aplicacionCbtis.ui.cursos.cuadernillos.VisoresPDF.VisorPDFGuiasEstudio;
 
-public class GuiasFragment extends Fragment implements RecyclerViewInterfaceGuias {
+public class GuiasFragment extends Fragment {
 
-    private ListAdapter listAdapter;
-    private ArrayList<String> listaElemtosGuias;
     private Context context;
     private StorageReference mStorageRef;
+    private ListView listViewGuias;
+    private ArrayList<String> guiasNombres;
 
     public GuiasFragment() {
     }
 
     public static GuiasFragment newInstance(String param1, String param2) {
         GuiasFragment fragment = new GuiasFragment();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
         return fragment;
     }
 
@@ -58,7 +62,9 @@ public class GuiasFragment extends Fragment implements RecyclerViewInterfaceGuia
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        this.listaElemtosGuias = new ArrayList<String>();
+        this.listViewGuias = view.findViewById(R.id.ListView_guias);
+        this.guiasNombres = new ArrayList<>();
+
 
         //Inicializando el objecto en FireBase
         this.mStorageRef = FirebaseStorage.getInstance().getReference();
@@ -66,24 +72,45 @@ public class GuiasFragment extends Fragment implements RecyclerViewInterfaceGuia
         //Llamo la referencia de bucket donde tengo los cuadernillos guardados en Firebase
         StorageReference reference = this.mStorageRef.child("Guias de estudio");
 
+        //Codificación para traer todos los titulos de los libros
         reference.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>(){
             @Override
             public void onSuccess(ListResult listResult) {
                 for (StorageReference item : listResult.getItems()) {
-                    listaElemtosGuias.add(item.getName() + "");
+                    guiasNombres.add(item.getName() + "");
                 }
+                //Configuración del adaptador de la lista
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, guiasNombres);
+
+                //Muestro el adaptador en la vista
+                listViewGuias.setAdapter(adapter);
+
+                listViewGuias.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                        final String titulo_guia_seleccionado = listViewGuias.getItemAtPosition(position).toString();
+
+                        Intent intent = new Intent(getContext(), VisorPDFGuiasEstudio.class);
+                        intent.putExtra("TITULO GUIA", titulo_guia_seleccionado);
+                        startActivity(intent);
+                    }
+                });
             }
+        }).addOnFailureListener(e -> {
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(getContext());
+            builder1.setMessage("Ha ocurrido un error al cargar los cuadernillos. Revisa tu conexión a internet.");
+            builder1.setCancelable(true);
+            builder1.setPositiveButton(
+                    "Ok",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.cancel();
+                        }
+                    });
+            AlertDialog alert1 = builder1.create();
+            alert1.show();
         });
-
-        ListAdapter listAdapter = new ListAdapter(this.listaElemtosGuias, getContext(), this);
-        RecyclerView recyclerView = view.findViewById(R.id.recycler_guias);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(listAdapter);
-   }
-
-    @Override
-    public void onItemClick(int position) {
-
     }
 }
--- app/src/main/java/cbtis/app/aplicacionCbtis/ui/cursos/cuadernillos/VisoresPDF/VisorPDFGuiasEstudio.java
+++ app/src/main/java/cbtis/app/aplicacionCbtis/ui/cursos/cuadernillos/VisoresPDF/VisorPDFGuiasEstudio.java
@@ -1,2 +1,43 @@
-package cbtis.app.aplicacionCbtis.ui.cursos.cuadernillos.VisoresPDF;public class VisorPDFGuiasEstudio {
+package cbtis.app.aplicacionCbtis.ui.cursos.cuadernillos.VisoresPDF;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import cbtis.app.aplicacionCbtis.R;
+
+public class VisorPDFGuiasEstudio extends AppCompatActivity {
+    public static final long ONE_MEGABYTE = 1024 * 1024*50;
+
+    private String nombreGuia;
+    private PDFView pdfView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visor_guias_estudio);
+
+        this.pdfView = findViewById(R.id.pdfView_guias_estudio);
+        this.nombreGuia = getIntent().getStringExtra("TITULO GUIA");
+
+        //Hacemos la conexión al FireBase Storage
+        FirebaseStorage mFireBaseStorage = FirebaseStorage.getInstance();
+        StorageReference mmFireBaseStorageRef = mFireBaseStorage.getReference().child("Guias de estudio");
+
+        mmFireBaseStorageRef.child(nombreGuia).getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                pdfView.fromBytes(bytes).load();
+            }
+        }).addOnFailureListener(e -> {
+            Toast.makeText(VisorPDFGuiasEstudio.this, "Descarga fallida", Toast.LENGTH_LONG).show();
+        });
+    }
 }
