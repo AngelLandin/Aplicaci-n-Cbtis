61f221719ed2303cadc377a9552e7a3b7062b12a
Angel Landin Lopez <angel.ezlan@gmail.com>
2023-11-05 22:58:35 -0600

    Version 1.0 Escuela Activa

--- app/src/main/java/cbtis/app/aplicacionCbtis/ui/comunidad/controlador/ClubsFragment.java
+++ app/src/main/java/cbtis/app/aplicacionCbtis/ui/comunidad/controlador/ClubsFragment.java
@@ -49,13 +49,13 @@ public class ClubsFragment extends Fragment {
 
         this.elementosClubsList = new ArrayList<>();
 
-        //Agregamos los clubs al RecyclerView
-        //Clubs deportivos.
+        //#-----------------------Agregamos los clubs al RecyclerView---------------------#
+        //Elementos de Clubs deportivos.
         this.elementosClubsList.add(new ListaElementosClubs(R.drawable.futbol, "Club de Futbol", "Juego entre dos equipos de once jugadores cada uno, cuyo objetivo es hacer entrar en la portería contraria un balón que no puede ser tocado con las manos ni con los brazos."));
         this.elementosClubsList.add(new ListaElementosClubs(R.drawable.basquet, "Club de Basquetbol", "El baloncesto o básquetbol es un deporte en el cual compiten dos equipos de cinco jugadores cada uno. El objetivo es introducir la pelota (balón) en el aro (cesta o canasta) del equipo contrario."));
         this.elementosClubsList.add(new ListaElementosClubs(R.drawable.voleibol, "Club de Voleibol", "El voleibol, es un deporte donde dos equipos se enfrentan sobre un terreno de juego liso separados por una red central, tratando de pasar el balón por encima de la red hacia el suelo del campo contrario."));
 
-        //Clubs culturales.
+        //Elementos de Clubs culturales.
         this.elementosClubsList.add(new ListaElementosClubs(R.drawable.danza, "Club de danza", "La danza es un movimiento puesto en forma rítmica y espacial, una sucesión de movimientos que comienza, se desarrolla y finaliza."));
         this.elementosClubsList.add(new ListaElementosClubs(R.drawable.dibujo, "Club de dibujo", "El Club De Dibujo fue creado para todas las personas que están interesadas en dibujar, ya sea como hobby o como parte de su profesión."));
         this.elementosClubsList.add(new ListaElementosClubs(R.drawable.ciencias, "Club de Ciencias", "Lo forman un grupo de jóvenes con una organización establecida que, orientados por un adulto, desarrollan actividades que favorecen la educación científica y tecnología del grupo y de la comunidad, contribuyendo a una mejor comprensión de la función de la ciencia y la tecnología en el momento actual."));
@@ -63,7 +63,6 @@ public class ClubsFragment extends Fragment {
         this.elementosClubsList.add(new ListaElementosClubs(R.drawable.ecologia, "Club de ecologia", "Los jovenes que integran este club se encargan de elaborar y desarrollar labores de promoción relacionadas con la cultura ecológica"));
         this.elementosClubsList.add(new ListaElementosClubs(R.drawable.teatro, "Club de teatro", "El club de teatro tiene el propósito de generar un espacio de convivencia para la formación teatral, la comunicación a través del teatro, el intercambio entre teatristas y público."));
         this.elementosClubsList.add(new ListaElementosClubs(R.drawable.ajedrez, "Club de ajedrez", "El club de ajedrez es un club formado con el propósito de jugar el juego de mesa del ajedrez."));
-        this.elementosClubsList.add(new ListaElementosClubs(R.drawable.robotica, "Club de robotica", "El Club de Robótica es un espacio de experimentación, en el que se aplican estrategias tanto de aprendizaje activo como construccionista."));
         this.elementosClubsList.add(new ListaElementosClubs(R.drawable.banda_guerra, "Club de banda de guerra", "Es una disciplina que se practica por medio de cornetas y clarines, se hace acompañar con caseros, la cual está constituida por 12 caseros, 12 cornetas, una corneta y un comandante."));
 
         ListAdapterClubs listAdapterClubs = new ListAdapterClubs(elementosClubsList, getContext());
